abe1590e3772351ea1673fdd43e81f5f37d42839
Heidi Wikman <heidi.wikman@kapsi.fi>
2022-08-04 16:02:14 +0300

    Add project files.

---
+++ JokeApp/ApiHelper.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Text.Json;
+using HealthApp;
+
+namespace APIHelpers
+{
+
+    public static class JokeApi
+    {
+        const string url = "https://v2.jokeapi.dev/joke/";
+
+        public static async Task<Joke> GetRandomJoke(string category)
+        {
+            string fullUrl = url + "/" + category;
+            Joke response = await ApiHelper.RunAsync<Joke>(fullUrl, "");
+            return response;
+        }
+    }
+
+
+    public static class ApiHelper
+    {
+        // create HTTP client
+        private static HttpClient GetHttpClient(string url)
+        {
+            var client = new HttpClient { BaseAddress = new Uri(url) };
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            return client;
+        }
+
+        public static async Task<T> RunAsync<T>(string url, string urlParams)
+        {
+            try
+            {
+                using (var client = GetHttpClient(url))
+                {
+                    // send GET request
+                    HttpResponseMessage response = await client.GetAsync(urlParams);
+
+                    if (response.StatusCode == HttpStatusCode.OK)
+                    {
+                        var json = await response.Content.ReadAsStringAsync();
+
+                        // JSON to an object
+                        var result = JsonSerializer.Deserialize<T>(json);
+                        return result;
+                    }
+
+                    return default(T);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                return default(T);
+            }
+        }
+    }
+}
---
+++ JokeApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using APIHelpers;
+
+
+namespace HealthApp
+{
+    class Program
+    {
+        private static void PrintJoke(Joke joke)
+        {
+            Console.WriteLine(joke.joke);
+        }
+
+
+        private static async Task GetRandomJoke()
+        {
+            Console.WriteLine("  *** Hello from joke app *** \n");
+            Console.Write("\nGive category: ");
+            string input = Console.ReadLine();
+
+            Joke joke = await JokeApi.GetRandomJoke(input);
+
+            PrintJoke(joke);
+        }
+
+
+        static void Main(string[] args)
+        {
+            GetRandomJoke();
+            Console.ReadKey(true);
+        }
+    }
+}
+
